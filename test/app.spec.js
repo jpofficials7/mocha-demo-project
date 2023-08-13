441930c67d557be355d22657a5d2df0a63e2f03c
jayant pal <jpofficials7@gmail.com>
2023-08-13 22:45:32 +0530

    Synchronous Test Cases describe, it, context, specify, suite and test - BDD & TDD

---
+++ test/app.spec.js
@@ -0,0 +1,24 @@
+const { add, sub } = require('../src/app');
+const expect = require('chai').expect;
+
+//BDD
+describe('suite 1', () => {
+    it('add(2, 3) should return 5', () => {
+        expect(add(2, 3)).to.be.equal(5);
+    })
+});
+
+context('suite 2', () => {
+    specify('add(3, 3) should return 6', () => {
+        expect(add(3, 3)).to.be.equal(6);
+    })
+});
+
+//TDD
+const { suite, test } = require('mocha');
+
+suite('suite 2', () => {
+    test('add(3, 3) should return 6', () => {
+        expect(add(3, 3)).to.be.equal(6);
+    })
+});
