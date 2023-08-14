f15089c404908f1e6fa4f625a3814c5040c3bc1e
jayant pal <jpofficials7@gmail.com>
2023-08-14 19:04:38 +0530

    run async test

---
+++ test/async.app.spec.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect;
+const axios = require('axios');
+
+//BDD
+describe('async test suite', () => {
+    it('promised based way', () => {
+        return axios.get('https://reqres.in/api/users/2').then(res => {
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in');
+        })
+    });
+
+    it('done based way', (done) => {
+        axios.get('https://reqres.in/api/users/2').then(res => {
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in');
+            done();
+        }).catch(err => {
+            done(err);
+        })
+    });
+    it('async await based way', async () => {
+        const res = await axios.get('https://reqres.in/api/users/2');
+        expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in');
+    });
+})
+
+//TDD
