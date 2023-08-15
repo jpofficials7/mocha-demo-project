86c1f2fa7e56c4e5cac499260e509b5193b97316
jayant pal <jpofficials7@gmail.com>
2023-08-16 01:41:38 +0530

    sinon mock

--- src/calc.js
+++ src/calc.js
@@ -1,17 +1,9 @@
 class Calculator {
-    constructor(name) {
-        this.property = name;
-    }
-    get calc(){
-        return this.property;
-    }
-    set calc(name){
-        this.property = name;
-    }
-    myCalc(a,b,callback){
-        callback(a,b);
+    printHi(msg){
+        console.log(msg);
     }
     sum(a, b) {
+        this.printHi('Qa Box');
         return a + b;
     }
     sub(a, b) {
