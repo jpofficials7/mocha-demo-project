3873ab264e8ed3f302bafe8db11478958d446c9b
jayant pal <jpofficials7@gmail.com>
2023-08-15 16:30:57 +0530

    mochawesome

--- .mocharc.js
+++ .mocharc.js
@@ -1,10 +1,8 @@
-// File where we can control configuration of mocha
+// // // File where we can control configuration of mocha
 module.exports = {
-    ui: 'tdd',
+    ui: 'bdd',
     timeout: 2000,
     slow: 150,
-    reporter: 'spec',
     retries: 0,
-    ui: 'bdd',
     'watch-files': ['test/**/*.js'],
 }
