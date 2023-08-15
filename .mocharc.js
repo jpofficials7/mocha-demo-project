93711d61dd06ab33266242451ab27e7bad3a5e02
jayant pal <jpofficials7@gmail.com>
2023-08-15 15:59:57 +0530

    retry timeout skip only

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,10 @@
 // File where we can control configuration of mocha
 module.exports = {
-    // ui: 'tdd'
+    ui: 'tdd',
+    timeout: 2000,
+    slow: 150,
+    reporter: 'spec',
+    retries: 0,
+    ui: 'bdd',
+    'watch-files': ['test/**/*.js'],
 }
--- test/async.app.spec.js
+++ test/async.app.spec.js
@@ -2,10 +2,35 @@ const expect = require('chai').expect;
 const axios = require('axios');
 
 //BDD
-describe('async test suite', () => {
-    it('promised based way', () => {
+describe.only('async test suite', function(){
+
+    it.skip('promised based way', () => {
+        this.timeout(100);
         return axios.get('https://reqres.in/api/users/2').then(res => {
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.int');
+        })
+    });
+
+    it('done based way', (done) => {
+        axios.get('https://reqres.in/api/users/2').then(res => {
             expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in');
+            done();
+        }).catch(err => {
+            done(err);
+        })
+    });
+    it.skip('async await based way', async () => {
+        const res = await axios.get('https://reqres.in/api/users/2');
+        expect(res.data.data.email).to.be.equal('janet.weaver@reqres.in');
+    });
+})
+
+describe('async test suite', function(){
+
+    it('promised based way', () => {
+        this.timeout(100);
+        return axios.get('https://reqres.in/api/users/2').then(res => {
+            expect(res.data.data.email).to.be.equal('janet.weaver@reqres.int');
         })
     });
 
